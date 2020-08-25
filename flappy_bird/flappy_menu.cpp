82c06e37dbd2b10e66182bd1dc79c6d6feb289fd
noaione <noaione0809@gmail.com>
2020-08-25 13:19:23 +0700

    flappy_bird: move from outer folder to inner folder also add it into main

---
+++ flappy_bird/flappy_menu.cpp
@@ -0,0 +1,164 @@
+#include <iso646.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#include <conio.h>
+#else
+#include <ncurses.h>
+#include "../extras/unix_support.h"
+#endif
+
+#include "flappygame.h"
+#include "../extras/extra.h"
+
+#define keyUp 72 //arrow keys
+#define keyDown 80
+#define keyLeft 75
+#define keyRight 77
+#define keyEnter 13
+
+void screenTitle(int *num)
+{
+	int titleScreen[10][94] = {{1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1},
+							   {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1},
+							   {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+							   {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+							   {1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0},
+							   {1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0},
+							   {1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+							   {1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+							   {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1},
+							   {1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1}};
+
+	setCursorPosition(0, 0);
+
+	puts("");
+	for (int i = 0; i < 10; i++)
+	{
+		printf("     ");
+		for (int j = 0; j < 94; j++)
+		{
+			if (titleScreen[i][j])
+			{
+				int state = (j + *num + 3 * i) % 69;
+				if ((0 <= state and state <= 9) or (60 <= state and state <= 69))
+				{
+					printf("%c", char(176));
+				}
+				else if ((10 <= state and state <= 19) or (50 <= state and state <= 59))
+				{
+					printf("%c", char(177));
+				}
+
+				else if ((20 <= state and state <= 29) or (40 <= state and state <= 49))
+				{
+					printf("%c", char(178));
+				}
+				else
+				{
+					printf("%c", char(219));
+				}
+			}
+			else
+				printf(" ");
+		}
+		puts("");
+		hideCursor();
+	}
+}
+
+// N4O: Renamed to avoid conflict.
+int flappy_bird()
+{
+	char key;
+	int x = 1;
+	int num;
+	int currPos = 0;
+	char options[3][15] = {"PLAY", "Highscore", "Exit"};
+	do
+	{
+		screenTitle(&num);
+		num += 68;
+		printf("                       ");
+		for (int i = 0; i < 30; i++)
+		{
+			if (i == 0)
+				printf("%c", char(201));
+			else if (i == 29)
+				printf("%c", char(187));
+			else
+				printf("%c", char(205));
+		}
+		puts("");
+		printf("                       %c                            %c\n", char(186), char(186));
+		for (int i = 0; i < 3; i++)
+		{
+			printf("                       %c   ", char(186));
+			if (currPos == i)
+				printf(">>   ");
+			else
+				printf("     ");
+
+			printf("%s", options[i]);
+			for (int j = strlen(options[i]); j < 12; j++)
+				printf(" ");
+
+			if (currPos == i)
+				printf("   <<");
+			else
+				printf("     ");
+			printf("   %c", char(186));
+			puts("");
+		}
+		printf("                       %c                            %c\n", char(186), char(186));
+		printf("                       ");
+		for (int i = 0; i < 30; i++)
+		{
+			if (i == 0)
+				printf("%c", char(200));
+			else if (i == 29)
+				printf("%c", char(188));
+			else
+				printf("%c", char(205));
+		}
+		if (kbhit())
+		{
+			key = getch();
+			if ((key == keyUp or key == keyLeft) and currPos != 0)
+				currPos--;
+			else if ((key == keyDown or key == keyRight) and currPos != 6)
+				currPos++;
+			else if (key == keyEnter)
+			{
+				switch (currPos)
+				{
+				case 0:
+				{
+					clearScreen();
+					startup_bird(); // Data initialization
+					while (1)
+					{
+						show_bird();		  // display
+						updateWithoutInput(); // Updates unrelated to user input
+						updateWithInput();	  // Updates related to user input
+					}
+				}
+				case 1:
+				{
+				}
+				case 2:
+				{
+					clearScreen();
+					printf("You have clicked exit");
+					return 0;
+				}
+				}
+			}
+		}
+
+	} while (x != 0);
+
+	return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include "extras/extra.h"
 
 // Import pong.
 #include "pong/ponggame.cpp"
+// Import flappy bird.
+// Note: Doesn't supppot Linux completely.
+// someone please reimplement getch()
+#include "flappy_bird/flappy_menu.cpp"
 
+// N4O: someone please make the main menu, kthx.
+// extra.h contain some function like
+// hideCursor(), clearScreen(), setCursorPosition(x, y)
+// please use that since it's UNIX/Windows compliant.
 int main() {
+    // flappy_bird();
     return 0;
 }
