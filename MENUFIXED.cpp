d26fc40873233b7f266bb3c441a8c895b0f37f54
4shb0rne <70153302+4shb0rne@users.noreply.github.com>
2020-08-25 12:11:04 +0700

    Add files via upload

---
+++ MENUFIXED.cpp
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <conio.h>
+#include "flappygame.h"
+
+#define keyUp 72  //arrow keys
+#define keyDown 80
+#define keyLeft 75
+#define keyRight 77
+#define keyEnter 13
+
+void screenTitle(int *num) 
+{
+	int titleScreen [10][94] = {{1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1},
+	                    {1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1},
+	                    {1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0},
+	                    {1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0},
+	                    {1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,1,1,0,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,0,0},
+	                    {1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,1,1,0,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,0,0},
+	                    {1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,1,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0},
+	                    {1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,1,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0},
+	                    {1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1},
+	                    {1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1}};
+
+ 
+	COORD cursorPosition; 
+	cursorPosition.X = 0; 
+	cursorPosition.Y = 0;	
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
+
+
+	puts(""); 
+	for(int i = 0; i < 10; i++) 
+	{ 
+		printf("     ");
+		for(int j = 0; j < 94; j++) 
+		{
+			if(titleScreen[i][j]) 
+			{ 
+				int state = (j + *num + 3 * i) % 69;
+				if((0 <= state and state <= 9) or (60 <= state and state <= 69)) 
+				{
+					printf("%c", char(176));
+				}
+				else if((10 <= state and state <= 19) or (50 <= state and state <= 59)) 
+				{
+					printf("%c", char(177));
+				}
+				
+				else if((20 <= state and state <= 29) or (40 <= state and state <= 49)) 
+				{
+					printf("%c", char(178));
+				}
+				else 
+				{
+					printf("%c", char(219));
+				}
+			}
+			else printf(" ");
+		}
+		puts("");
+		HideCursor();
+	}
+}
+int main()
+{
+	char key;
+	int x = 1;
+	int num;
+	int currPos = 0;
+	char options[3][15] = {"PLAY", "Highscore", "Exit"};
+	do
+	{
+		screenTitle(&num);	
+		num += 68;
+		printf("                       ");
+		for(int i = 0; i < 30; i++) {
+			if(i == 0) printf("%c", char(201));
+			else if(i == 29) printf("%c", char(187));
+			else printf("%c", char(205));
+		}
+		puts("");
+		printf("                       %c                            %c\n", char(186), char(186));
+		for(int i = 0; i < 3; i++) 
+		{
+			printf("                       %c   ", char(186));
+			if(currPos == i) printf(">>   ");	
+			else printf("     ");
+			
+			printf("%s", options[i]); 
+			for(int j = strlen(options[i]); j < 12; j++) printf(" "); 
+
+			if(currPos == i) printf("   <<");
+			else printf("     ");
+			printf("   %c", char(186));
+			puts("");
+		}
+		printf("                       %c                            %c\n", char(186), char(186));
+		printf("                       ");
+		for(int i = 0; i < 30; i++) {
+			if(i == 0) printf("%c", char(200)); 
+			else if(i == 29) printf("%c", char(188));
+			else printf("%c", char(205));
+		}
+		if(kbhit()) {
+			key = getch();
+			if((key == keyUp or key == keyLeft) and currPos != 0) currPos--;
+			else if((key == keyDown or key == keyRight) and currPos != 6) currPos++;
+			else if(key == keyEnter) 
+			{
+				switch(currPos)
+				{
+					case 0 :
+					{
+						
+						system("cls");
+						startup(); // Data initialization  
+						while(1)
+						{
+							show(); // display 
+							updateWithoutInput(); // Updates unrelated to user input 
+							updateWithInput(); // Updates related to user input 
+						}
+					}
+					case 1 :
+					{
+								
+					}
+					case 2 :
+					{
+						system("cls");
+						printf("You have clicked exit");
+						return 0;
+					}
+			}
+		}
+	}
+
+	} while(x != 0);
+   
+   
+	
+    return 0;
+}
