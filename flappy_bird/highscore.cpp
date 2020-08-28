666fe311d664062e9946c8e3f5a53a13ac6274b3
noaione <noaione0809@gmail.com>
2020-08-28 23:41:19 +0700

    flappy_bird: fix problem with highscore system
    Signed-off-by: noaione <noaione0809@gmail.com>

--- flappy_bird/highscore.cpp
+++ flappy_bird/highscore.cpp
@@ -20,12 +20,7 @@ void swap(highscore *a, highscore *b) {
 
 void listName() {
 	FILE *fptr;
-	char *flapScoreName = getSaveGameFolder();
-	strcat(flapScoreName, "flappybird/");
-	checkAndCreateFolder(flapScoreName);
-	strcat(flapScoreName, "highscore_name.txt");
-	checkAndCreateDummy(flapScoreName);
-	fptr = fopen(flapScoreName, "r");
+	fptr = fopen("flappy_scorename.txt", "r");
 
 	currIdx = 0;
 	while(fscanf(fptr, "%s", leaderboard[currIdx].name) != EOF) {
@@ -37,12 +32,7 @@ void listName() {
 
 void listScore() {
 	FILE *fptr;
-	char *flapScore = getSaveGameFolder();
-	strcat(flapScore, "flappybird/");
-	checkAndCreateFolder(flapScore);
-	strcat(flapScore, "highscore_score.txt");
-	checkAndCreateDummy(flapScore);
-	fptr = fopen(flapScore, "r");
+	fptr = fopen("flappy_scores.txt", "r");
 
 	currIdx = 0;
 	while(fscanf(fptr, "%d", &leaderboard[currIdx].score) != EOF) {
@@ -52,7 +42,7 @@ void listScore() {
 	fclose(fptr);
 }
 
-void processScore(highscore a) { // masukkan a ke leaderboard, sort.
+void processScore(highscore a, bool dont_save = false) { // masukkan a ke leaderboard, sort.
 	FILE *fptr1, *fptr2;
 
 	listName();
@@ -68,24 +58,18 @@ void processScore(highscore a) { // masukkan a ke leaderboard, sort.
 		}
 	}
 
-	char *flapScore = getSaveGameFolder();
-	char *flapScoreName;
-	strcat(flapScore, "flappybird/");
-	checkAndCreateFolder(flapScore);
-	strcpy(flapScoreName, flapScore);
-	strcat(flapScore, "highscore_score.txt");
-	strcat(flapScoreName, "highscore_name.txt");
+	if (dont_save) {
+		fptr1 = fopen("flappy_scorename.txt", "w");
+		fptr2 = fopen("flappy_scores.txt", "w");
 
-	fptr1 = fopen(flapScoreName, "w");
-	fptr2 = fopen(flapScore, "w");
+		for(int i = 0; i < currIdx; i++) {
+			fprintf(fptr1, "%s\n", leaderboard[i].name);
+			fprintf(fptr2, "%d\n", leaderboard[i].score);
+		}
 
-	for(int i = 0; i < currIdx; i++) {
-		fprintf(fptr1, "%s\n", leaderboard[i].name);
-		fprintf(fptr2, "%d\n", leaderboard[i].score);
+		fclose(fptr1);
+		fclose(fptr2);
 	}
-
-	fclose(fptr1);
-	fclose(fptr2);
 }
 
 void inputUsername(int score) {
@@ -123,42 +107,26 @@ void inputUsername(int score) {
 	highscore player;
 	strcpy(player.name, username);
 	player.score = score;
-	processScore(player);
+	processScore(player, true);
 }
 
-void printLeaderboardTitle(int *num) {
+void printLeaderboardTitle() {
 	int title[5][69] = {{1,1,0,0,0,1,1,0,1,1,0,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0},
 						{1,1,0,0,0,1,1,0,1,1,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,1,1,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,1,1,0,0,0,1,1,0,1,1,0,0,0,0,0,0},
 						{1,1,1,1,1,1,1,0,1,1,0,1,1,0,0,0,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,1,1,1,1,1,1,0,0,1,1,1,1,1,0,0,0},
 						{1,1,0,0,0,1,1,0,1,1,0,1,1,0,0,0,0,1,1,0,1,1,0,0,0,1,1,0,0,0,0,0,0,1,1,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,1,1,0,0,0,1,1,0,1,1,0,0,0,0,0,0},
 						{1,1,0,0,0,1,1,0,1,1,0,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0,1,1,1,1,1,1,1,0}};
 
-	clearScreen();
-	hideCursor(); // hide cursor
-	// set cursor ke 0, 0
-	setCursorPosition(0, 0); // gunanya sebagai alternatif dari clear screen, tapi lebih cepat	
-
-
+	hideCursor();
 	puts(""); // kasih jarak dari pojok atas
 	for(int i = 0; i < 5; i++) { // print tulisan hangman
 		printf("\t");
 		for(int j = 0; j < 69; j++) {
 			if(title[i][j]) { // kalau titleScreen[i][j] = 1 --> merupakan bagian text
-				int state = (j + *num + 3 * i) % 69;
-				if((0 <= state and state <= 9) or (60 <= state and state <= 69)) {
-					printf("%c", char(176));
-				}
-				else if((10 <= state and state <= 19) or (50 <= state and state <= 59)) {
-					printf("%c", char(177));
-				}
-				else if((20 <= state and state <= 29) or (40 <= state and state <= 49)) {
-					printf("%c", char(178));
-				}
-				else {
-					printf("%c", char(219));
-				}
+				printf("#");
+			} else {
+				printf(" ");
 			}
-			else printf(" ");
 		}
 		puts("");
 	}
@@ -171,54 +139,54 @@ void printLeaderboard() {
 	highscore dummy; // trigger dia process leaderboard
 	strcpy(dummy.name, "------");
 	dummy.score = -1;
-	processScore(dummy);
+	processScore(dummy, false);
 	
 	char s[255];
-	int num = 0;
-
-	printLeaderboardTitle(&num);
-	printf("\t");
-	for(int i = 0; i < 68; i++) printf("=");
-	puts("");
-	for(int i = 0; i < 10; i++) {
-		if(strcmp(leaderboard[i].name, "------") != 0) {
-			printf("\t");
-			for(int i = 0; i < 68; i++) {
-				if(i == 0) printf("%c", char(201));
-				else if(i == 33) printf("%c", char(203));
-				else if(i == 67) printf("%c", char(187));
-				else printf("%c", char(205));
-			}
-			puts("");
-
-			printf("\t%c", char(186));
-			printf("  %s", leaderboard[i].name);
-
-			int len = strlen(leaderboard[i].name);
-			for(int j = len; j < 16; j++) printf(" ");
-			printf("\t\t %c                            ", char(186));
-
-			printf("%03d  ", leaderboard[i].score);
-			printf("%c", char(186));
-			puts("");
-
-			printf("\t");
-			for(int i = 0; i < 68; i++) {
-				if(i == 0) printf("%c", char(200));
-				else if(i == 33) printf("%c", char(202));
-				else if(i == 67) printf("%c", char(188));
-				else printf("%c", char(205));
-			}
-			puts("");
-		}
-	}
-
-	puts("\n");
-	puts("\t\t\t     Press Enter to go back.");
+	int is_first = 1;
 
 	do {
-		printLeaderboardTitle(&num);
-		num += 1;
+		if (is_first) {
+			printLeaderboardTitle();
+			printf("\t");
+			for(int i = 0; i < 68; i++) printf("=");
+			puts("");
+			for(int i = 0; i < 10; i++) {
+				if(strcmp(leaderboard[i].name, "------") != 0) {
+					printf("\t");
+					for(int i = 0; i < 68; i++) {
+						if(i == 0) printf("%c", char(201));
+						else if(i == 33) printf("%c", char(203));
+						else if(i == 67) printf("%c", char(187));
+						else printf("%c", char(205));
+					}
+					puts("");
+
+					printf("\t%c", char(186));
+					printf("  %s", leaderboard[i].name);
+
+					int len = strlen(leaderboard[i].name);
+					for(int j = len; j < 16; j++) printf(" ");
+					printf("\t\t %c                            ", char(186));
+
+					printf("%03d  ", leaderboard[i].score);
+					printf("%c", char(186));
+					puts("");
+
+					printf("\t");
+					for(int i = 0; i < 68; i++) {
+						if(i == 0) printf("%c", char(200));
+						else if(i == 33) printf("%c", char(202));
+						else if(i == 67) printf("%c", char(188));
+						else printf("%c", char(205));
+					}
+					puts("");
+				}
+			}
+
+			puts("\n");
+			puts("\t\t\t     Press Enter to go back.");
+			is_first = false;
+		}
 
 		if(kbhit()) {
 			if(getch() == keyEnter) {
