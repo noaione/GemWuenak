f9c9217d299fc77164ca136dfb39098037b92905
Demonsta18 <68661328+Demonsta18@users.noreply.github.com>
2020-08-29 15:42:50 +0700

    Update ponggame.cpp

--- pong/ponggame.cpp
+++ pong/ponggame.cpp
@@ -146,6 +146,7 @@ class Ping_Pong{ //Main var
 				puts("");
 				if (player == player_1) puts("PLAYER 1 WINS!!"); 
 				else puts("PLAYER 2 WINS!!"); 
+				terminate = true;
 			}else{
 				ball->reset_ball(); 
 				player_1->blade_reset(); 
